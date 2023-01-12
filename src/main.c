3d7c6dd42bfdcfb6ccd02d1e66cbcad991cfc4b7
Marcus Kellerman <sharkus@gmail.com>
2023-01-11 16:52:41 -0800

    Refactored state machine. Untested.

--- src/main.c
+++ src/main.c
@@ -48,6 +48,13 @@ UINP_GPAD_DEV uinp_gpads[GPADSNUM];
 INP_XARC_DEV xarcdev;
 int use_syslog = 0;
 
+enum key_state {
+    key_up = 0,
+    key_down = 1,
+    key_down_filtered,
+    hot_key_sent
+};
+
 #define SYSLOG(...) if (use_syslog == 1) { syslog(__VA_ARGS__); }
 
 static void teardown();
@@ -75,7 +82,7 @@ static void send_key(const key_map_t *p_map_entry, uint16_t ev_value)
 int main(int argc, char* argv[]) {
 	int result = 0;
 	int rd, ctr;
-	char keyStates[KEY_MAP_SIZE];
+	enum key_state keyStates[KEY_MAP_SIZE];
 	char* evdev = NULL;
 
 	key_map_t key_map[KEY_MAP_SIZE];
@@ -144,7 +151,6 @@ int main(int argc, char* argv[]) {
 		}
 
 		for (ctr = 0; ctr < rd; ctr++) {
-			int skip_key_up_count = 0;
 			key_map_t *p_map_entry = NULL;
 			key_map_t *p_hk_entry = NULL;
 
@@ -154,49 +160,82 @@ int main(int argc, char* argv[]) {
 				continue;
 			if (EV_KEY == xarcdev.ev[ctr].type) {
 
-				keyStates[xarcdev.ev[ctr].code] = xarcdev.ev[ctr].value;
 				p_map_entry = &key_map[xarcdev.ev[ctr].code];
 				p_hk_entry = &hotkey_map[xarcdev.ev[ctr].code];
 
 				if (!p_map_entry->hot_key) {
+                    printf("Normal key %d detected: %d\n", p_map_entry->code, xarcdev.ev[ctr].code);
+                    /* There is no hot_key associated with this key so we can
+                     * blindly pass through all events */
                     send_key(p_map_entry, xarcdev.ev[ctr].value);
+                    keyStates[xarcdev.ev[ctr].code] = xarcdev.ev[ctr].value;
                 } else {
-					if (xarcdev.ev[ctr].value) {
-						/* Ignore all key down events for hotkeys */
-						continue;
-					}
-					if (keyStates[p_hk_entry->hot_key]) {
-						/* The main hotkey associated with this key
-						   is in keydown state so we have a hotkey hit! */
-						send_key(p_hk_entry, 1);
-						if (p_hk_entry->destination == game_pad) {
+                    printf("Hot key %d (%d) detected: %d\n", p_map_entry->code, p_map_entry->hot_key, xarcdev.ev[ctr].code);
+                    if (xarcdev.ev[ctr].value == 1) { /* down event */
+                        printf("  Key down detected");
+                        /* if this is a hotkey we always filter the downstroke.
+                         * if this key can be assigned to a hotkey, we need to filter
+                         * all down presses if the associated hotkey is depressed.
+                         */
+                        if (p_map_entry->hot_key == 1 || keyStates[p_map_entry->hot_key] == key_down) {
+                            /* filter down stroke .. (don't send anything for now)
+                             * We want to track that the hotkey is depressed,
+                             * the assigned key state does not change
+                             */
+                            if (p_map_entry->hot_key == 1) {
+                                printf("  Main hot_key detected, marking as filtered\n");
+                                keyStates[xarcdev.ev[ctr].code] = key_down_filtered;
+                            }
+                        } else {
+                            printf("  Hotkey not depressed, sending key.\n");
+                            send_key(p_map_entry, xarcdev.ev[ctr].value);
+                            keyStates[xarcdev.ev[ctr].code] = key_down;
+                        }
+                    } else if (xarcdev.ev[ctr].value == 0) { /* up event */
+                        printf("  Key up detected\n");
+                        key_map_t *p_entry = NULL;
+
+                        /* First check if this is a hotkey, in which case we
+                         * need to check if a hotkey had been sent, in which
+                         * case we don't need to do anything.
+                         */
+                        switch (keyStates[xarcdev.ev[ctr].code]) {
+                        case key_down:
+                            send_key(p_map_entry, xarcdev.ev[ctr].value);
+                            keyStates[xarcdev.ev[ctr].code] = key_up;
+                            continue;
+                        case hot_key_sent:
+                            keyStates[xarcdev.ev[ctr].code] = key_up;
+                            continue;
+                        case key_up:
+                            /* This key is tied to a hot key and this trigger
+                             * means we should send the special key as long as
+                             * the hot key is still depressed
+                             */
+                            if (keyStates[p_map_entry->hot_key] != key_up) {
+                                keyStates[p_map_entry->hot_key] = hot_key_sent;
+                                p_entry = p_hk_entry;
+                            } else {
+                                p_entry = p_map_entry;
+                            }
+                            break;
+                        case key_down_filtered:
+                            /* We filtered this key, but never got a paired
+                             * key, so we can send the normal key
+                             */
+                            p_entry = p_map_entry;
+                            break;
+                        default:
+                            break;
+                        }
+						send_key(p_entry, 1);
+						if (p_entry->destination == game_pad) {
 							uinput_gpad_sleep();
 						} else {
 							uinput_kbd_sleep();
 						}
-						send_key(p_hk_entry, 0);
-						/* TODO: verify this needs to be 2, this current behavior
-						is modeled after the existing code.
-						*/
-						skip_key_up_count = 2;
-                    } else {
-                        /* not a hotkey hit */
-                        if (skip_key_up_count > 0) {
-                            /* We are still in recovery from the last hotkey hit,
-                                so we are going to ignore this keyup event. */
-                            skip_key_up_count -= 1;
-                        } else {
-                            /* Since we filtered the key down event, we need to
-                               create both down and up events.
-                            */
-                            send_key(p_map_entry, 1);
-                            if (p_map_entry->destination == game_pad) {
-                                uinput_gpad_sleep();
-                            } else {
-                                uinput_kbd_sleep();
-                            }
-                            send_key(p_map_entry, 0);
-                        }
+						send_key(p_entry, 0);
+                        keyStates[p_entry->hot_key] = key_up;
                     }
                 }
             }
